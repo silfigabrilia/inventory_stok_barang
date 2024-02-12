e33994fb57558a5c425012292e13b46d8e9d3aec
silfigabrilia <66557176+silfigabrilia@users.noreply.github.com>
2024-02-12 13:44:05 +0700

    stok

---
+++ mdr/application/views/auth/login.php
@@ -0,0 +1,27 @@
+<div class="content">
+    <div class="brand">
+        <a class="link" href="<?= base_url('auth') ?>">AdminCAST</a>
+    </div>
+
+    <?= $this->session->flashdata('message') ?>
+    <form id="login-form" action="<?= base_url('auth') ?>" method="post">
+        <h2 class="login-title">Log in</h2>
+        <div class="form-group">
+            <div class="input-group-icon right">
+                <div class="input-icon"><i class="fa fa-envelope"></i></div>
+                <input type="text" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email" name="email" value="<?= set_value('email')  ?>">
+                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group-icon right">
+                <div class="input-icon"><i class="fa fa-lock font-16"></i></div>
+                <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" name="password" placeholder="Password">
+                <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+            </div>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-info btn-block" type="submit">Login</button>
+        </div>
+    </form>
+</div>
